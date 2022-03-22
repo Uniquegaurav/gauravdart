d43f819dbe45e1a791b1c0a6acdd2b3101e1d05e
Kumar Gaurav <k.gauravsingh.9234.gs@gmail.com>
2022-03-22 22:19:29 +0530

    input unit test cases modified

--- cli/test/input_test.dart
+++ cli/test/input_test.dart
@@ -1,113 +1,50 @@
+import 'package:cli/exceptions/exceptions.dart';
 import 'package:cli/util/validate_input.dart';
 import 'package:test/test.dart';
 
 void main() {
   test('Validating name 1', () {
-    String exceptionMsg = 'NO EXCEPTION';
-    try {
-      ValidateUtil.validateName('b');
-    } catch (e) {
-      exceptionMsg = e.toString();
-    }
-    expect(exceptionMsg, "Name can't be just one character");
+    expect(() => ValidateUtil.validateName(null),
+        throwsA(isA<ItemNullException>()));
   });
   test('Validating name 2', () {
-    String exceptionMsg = 'NO EXCEPTION';
-    try {
-      ValidateUtil.validateName('gaer');
-    } catch (e) {
-      exceptionMsg = e.toString();
-    }
-    expect(exceptionMsg, 'NO EXCEPTION');
+    expect(() => ValidateUtil.validateName('d'),
+        throwsA(isA<ItemNameException>()));
   });
   test('Validating name 3', () {
-    String exceptionMsg = 'NO EXCEPTION';
-    try {
-      ValidateUtil.validateName('56ag had');
-    } catch (e) {
-      exceptionMsg = e.toString();
-    }
-    expect(exceptionMsg, 'NO EXCEPTION');
+    expect(() => ValidateUtil.validateName('drtrt'), returnsNormally);
   });
   test('Validating type 1', () {
-    String exceptionMsg = 'NO EXCEPTION';
-    try {
-      ValidateUtil.validateType('raw');
-    } catch (e) {
-      exceptionMsg = e.toString();
-    }
-    expect(exceptionMsg, 'NO EXCEPTION');
+    expect(() => ValidateUtil.validateType('material'),
+        throwsA(isA<ItemTypeException>()));
   });
   test('Validating type 2', () {
-    String exceptionMsg = 'NO EXCEPTION';
-    try {
-      ValidateUtil.validateType('lasjd');
-    } catch (e) {
-      exceptionMsg = e.toString();
-    }
-    expect(exceptionMsg, "Type can only be [raw , manufactured , imported]");
+    expect(() => ValidateUtil.validateType('imported'), returnsNormally);
   });
   test('Validating type 3', () {
-    String exceptionMsg = 'NO EXCEPTION';
-    try {
-      ValidateUtil.validateType('material');
-    } catch (e) {
-      exceptionMsg = e.toString();
-    }
-    expect(exceptionMsg, "Type can only be [raw , manufactured , imported]");
+    expect(() => ValidateUtil.validateType(null),
+        throwsA(isA<ItemNullException>()));
   });
   test('Validating quantity 1', () {
-    String exceptionMsg = 'NO EXCEPTION';
-    try {
-      ValidateUtil.validateQuantity(0);
-    } catch (e) {
-      exceptionMsg = e.toString();
-    }
-    expect(exceptionMsg, "Entered value is invalid");
+    expect(() => ValidateUtil.validateQuantity(-78),
+        throwsA(isA<ItemValueException>()));
   });
   test('Validating quantity 2', () {
-    String exceptionMsg = 'NO EXCEPTION';
-    try {
-      ValidateUtil.validateQuantity(-345);
-    } catch (e) {
-      exceptionMsg = e.toString();
-    }
-    expect(exceptionMsg, "Entered value is invalid");
+    expect(() => ValidateUtil.validateQuantity(0),
+        throwsA(isA<ItemValueException>()));
   });
   test('Validating quantity 3', () {
-    String exceptionMsg = 'NO EXCEPTION';
-    try {
-      ValidateUtil.validateQuantity(23);
-    } catch (e) {
-      exceptionMsg = e.toString();
-    }
-    expect(exceptionMsg, 'NO EXCEPTION');
+    expect(() => ValidateUtil.validateQuantity(5), returnsNormally);
   });
   test('Validating price 1', () {
-    String exceptionMsg = 'NO EXCEPTION';
-    try {
-      ValidateUtil.validatePrice(-56.9);
-    } catch (e) {
-      exceptionMsg = e.toString();
-    }
-    expect(exceptionMsg, "Entered value is invalid");
+    expect(() => ValidateUtil.validatePrice(-780.9),
+        throwsA(isA<ItemValueException>()));
   });
   test('Validating price 2', () {
-    String exceptionMsg = 'NO EXCEPTION';
-    try {
-      ValidateUtil.validatePrice(23);
-    } catch (e) {
-      exceptionMsg = e.toString();
-    }
-    expect(exceptionMsg, 'NO EXCEPTION');
+    expect(() => ValidateUtil.validatePrice(0),
+        throwsA(isA<ItemValueException>()));
   });
   test('Validating price 3', () {
-    String exceptionMsg = 'NO EXCEPTION';
-    try {
-      ValidateUtil.validatePrice(456.8);
-    } catch (e) {
-      exceptionMsg = e.toString();
-    }
-    expect(exceptionMsg, 'NO EXCEPTION');
+    expect(() => ValidateUtil.validatePrice(230), returnsNormally);
   });
 }
