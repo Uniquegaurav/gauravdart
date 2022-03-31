3a252b306d540ebd2b7971eac6406dca521dff24
Kumar Gaurav <k.gauravsingh.9234.gs@gmail.com>
2022-03-31 13:34:27 +0530

    assignment2 folder added

---
+++ assignment2/cli/lib/util/user_functions.dart
@@ -0,0 +1,21 @@
+class AllFunctions {
+  static void getNewUser() {
+    print('need to implement this');
+  }
+
+  static void deleteUserByRollNumber() {
+    print('need to implement this');
+  }
+
+  static void dispalyUserDetails() {
+    print('need to implement this');
+  }
+
+  static void saveToDisk() {
+    print('need to implement this');
+  }
+
+  static void promptForSavingChanges() {
+    print('need to implement this');
+  }
+}
